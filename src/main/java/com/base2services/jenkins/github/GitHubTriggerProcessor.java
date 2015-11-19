5c8b2bedfd77bd3df7a50f4d1fcc4e02678a80d0
Jesse Zoldak <zoldak@edx.org>
2015-11-19 16:25:47 -0500

    Add ability to pass a custom message format with job name and parameters for triggering.

--- src/main/java/com/base2services/jenkins/github/GitHubTriggerProcessor.java
+++ src/main/java/com/base2services/jenkins/github/GitHubTriggerProcessor.java
@@ -5,13 +5,25 @@ import com.base2services.jenkins.trigger.TriggerProcessor;
 import com.cloudbees.jenkins.GitHubRepositoryName;
 import com.cloudbees.jenkins.GitHubTrigger;
 import hudson.model.AbstractProject;
+import hudson.model.Cause;
+import hudson.model.CauseAction;
 import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.model.ParametersAction;
+import hudson.model.StringParameterValue;
 import hudson.security.ACL;
 import hudson.triggers.Trigger;
+import hudson.triggers.TriggerDescriptor;
+import jenkins.model.Jenkins;
+import jenkins.model.ParameterizedJobMixIn;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.context.SecurityContextHolder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,15 +39,23 @@ public class GitHubTriggerProcessor implements TriggerProcessor {
     private static final Logger LOGGER = Logger.getLogger(GitHubTriggerProcessor.class.getName());
 
     public void trigger(String payload) {
-        processGitHubPayload(payload, SqsBuildTrigger.class);
+        JSONObject json = extractJsonFromPayload(payload);
+        // You can signal through the payload that you will be using a custom format
+        // by including a root object named "custom_format" of any type and value.
+        if(json.has("custom_format")) {
+            processCustomPayload(json, SqsBuildTrigger.class);
+        }
+        // The default format, e.g. payload passed directly from a GitHub webhook,
+        // always includes a "repository" object.
+        else if(json.has("repository")) {
+            processGitHubPayload(json, SqsBuildTrigger.class);
+        }
+        else {
+            LOGGER.warning("sqs message contains unknown payload format");
+        }
     }
 
-    public void processGitHubPayload(String payload, Class<? extends Trigger> triggerClass) {
-        JSONObject json = extractJsonFromPayload(payload);
-        if(json == null) {
-            LOGGER.warning("sqs message contains unknown payload format");
-            return;
-        }
+    public void processGitHubPayload(JSONObject json, Class<? extends Trigger> triggerClass) {
         JSONObject repository = json.getJSONObject("repository");
         String repoUrl = repository.getString("url"); // something like 'https://github.com/kohsuke/foo'
         String repoName = repository.getString("name"); // 'foo' portion of the above URL
@@ -71,8 +91,65 @@ public class GitHubTriggerProcessor implements TriggerProcessor {
         }
     }
 
-    private JSONObject extractJsonFromPayload(String payload) {
-        JSONObject repository = null;
+    public void processCustomPayload(JSONObject json, Class<? extends Trigger> triggerClass) {
+        // Note that custom payloads will only trigger parameterized jobs that are configured with this SQS trigger.
+        // The custom payload must contain a root object named "job" of type string.
+        String jobToTrigger = json.getString("job");
+        if (jobToTrigger == null) {
+            LOGGER.warning("Custom sqs message payload does not contain data about the job to trigger.");
+            return;
+        }
+        // The custom payload must contain a root object named "parameters"
+        // that contains a list of parameters to pass to the job when scheduled.
+        // Each parameter object has information about the its type, name, and value.
+        Authentication old = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContextHolder.getContext().setAuthentication(ACL.SYSTEM);
+        try {
+            JSONArray parameters = json.getJSONArray("parameters");
+            Jenkins jenkins = Jenkins.getInstance();
+            for (Job job: jenkins.getAllItems(Job.class)) {
+                String jobName = job.getDisplayName();
+                if (jobName.equals(jobToTrigger)) {
+                    // Custom triggers operate on Parameterized jobs only
+                    if (job instanceof ParameterizedJobMixIn.ParameterizedJob) {
+                        // Make sure the job is configured to use the SQS trigger
+                        ParameterizedJobMixIn.ParameterizedJob pJob = (ParameterizedJobMixIn.ParameterizedJob) job;
+                        final Map<TriggerDescriptor, Trigger<?>> pJobTriggers = pJob.getTriggers();
+                        SqsBuildTrigger.DescriptorImpl descriptor = jenkins.getDescriptorByType(SqsBuildTrigger.DescriptorImpl.class);
+                        if (!pJobTriggers.containsKey(descriptor)) {
+                            LOGGER.warning("The job " + jobName + " is not configured to use the SQS trigger.");
+                        } else {
+                            final Job theJob = job;
+                            ParameterizedJobMixIn mixin = new ParameterizedJobMixIn() {
+                                @Override
+                                protected Job asJob() {
+                                    return theJob;
+                                }
+                            };
+                            Cause cause = new Cause.RemoteCause("SQS", "Triggered by SQS.");
+                            CauseAction cAction = new CauseAction(cause);
+                            // TODO this will only work with string parameters, and you will
+                            // need to pass in ALL parameters, as it will not fill in any defaults for you.
+                            List<ParameterValue> params = new ArrayList<ParameterValue>();
+                            for (int i = 0; i < parameters.size(); i++) {
+                                JSONObject param = (JSONObject)parameters.get(i);
+                                String name = param.optString("name", "xyz");
+                                String value = param.optString("value", "abc");
+                                StringParameterValue parameterValue = new StringParameterValue(name, value);
+                                params.add(parameterValue);
+                            }
+                            ParametersAction pAction = new ParametersAction(params);
+                            mixin.scheduleBuild2(0, cAction, pAction); // TODO check the return value for null / failure
+                        }
+                    }
+                }
+            }
+        } finally {
+            SecurityContextHolder.getContext().setAuthentication(old);
+        }
+    }
+
+    public JSONObject extractJsonFromPayload(String payload) {
         JSONObject json = JSONObject.fromObject(payload);
         if(json.has("Type")) {
             String msg = json.getString("Message");
@@ -83,10 +160,7 @@ public class GitHubTriggerProcessor implements TriggerProcessor {
                 }
                 return JSONObject.fromObject(msg);
             }
-        } else if (json.has("repository")){
-            return json;
-
         }
-        return null;
+        return json;
     }
 }
